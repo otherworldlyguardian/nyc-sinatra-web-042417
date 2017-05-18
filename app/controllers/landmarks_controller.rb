fbedc3c32b1972e593065eb787700154975dbf48
github username <github email address>
2017-05-18 14:08:12 -0400

    Began creating landmarks controller

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,50 +1,50 @@
 class LandmarksController < ApplicationController
 
-    get '/figures' do
+    get '/landmarks' do
+      @landmarks = Figure.all
+      erb :'/landmarks/index'
+    end
+
+
+    get '/landmarks/new' do
+      @figures = Landmark.all
+      @titles = Title.all
+      erb :'/landmarks/new'
+    end
+
+    post '/landmarks' do
+      figure = Landmark.new(params[:landmark])
+      if !params[:figure][:name].empty?
+        landmark.figures << Landmark.create(params[:figure])
+      end
+      if !params[:title][:name].empty?
+        landmark.titles << Title.create(params[:title])
+      end
+      landmark.save
+
+      redirect "/landmarks/#{landmark.id}"
+    end
+
+    get '/landmarks/:id' do
+      @landmark = Landmark.find(params[:id])
+      erb :'/landmarks/show'
+    end
+
+    get '/landmarks/:id/edit' do
+      @landmark = Landmark.find(params[:id])
       @figures = Figure.all
-      erb :'/figures/index'
-    end
-
-
-    get '/figures/new' do
-      @landmarks = Landmark.all
       @titles = Title.all
-      erb :'/figures/new'
+      erb :'/landmarks/edit'
     end
 
-    post '/figures' do
-      figure = Figure.new(params[:figure])
-      if !params[:landmark][:name].empty?
-        figure.landmarks << Landmark.create(params[:landmark])
+    patch '/landmarks/:id' do
+      landmark = Landmark.find(params[:id]).update(params[:figure])
+      if !params[:figure][:name].empty?
+        landmark.figures << Figures.create(params[:figure])
       end
       if !params[:title][:name].empty?
-        figure.titles << Title.create(params[:title])
+        landmark.titles << Title.create(params[:title])
       end
-      figure.save
-
-      redirect "/figures/#{figure.id}"
-    end
-
-    get '/figures/:id' do
-      @figure = Figure.find(params[:id])
-      erb :'/figures/show'
-    end
-
-    get '/figures/:id/edit' do
-      @figure = Figure.find(params[:id])
-      @landmarks = Landmark.all
-      @titles = Title.all
-      erb :'/figures/edit'
-    end
-
-    patch '/figures/:id' do
-      figure = Figure.find(params[:id]).update(params[:figure])
-      if !params[:landmark][:name].empty?
-        figure.landmarks << Landmark.create(params[:landmark])
-      end
-      if !params[:title][:name].empty?
-        figure.titles << Title.create(params[:title])
-      end
-      redirect "/figures/#{figure.id}"
+      redirect "/landmarks/#{landmark.id}"
     end
   end
